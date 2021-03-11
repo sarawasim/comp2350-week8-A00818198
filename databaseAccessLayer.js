2dff812d9675fcfd72cc5a9e1526c80fbc7b684d
Sara Wasim <sarawasim@hotmail.com>
2021-03-10 22:07:23 -0800

    fixed errors

--- databaseAccessLayer.js
+++ databaseAccessLayer.js
@@ -10,13 +10,13 @@ function getAllUsers(callback) {
 		else {
 			console.log(results);
 			callback(null, results);
-		}		
+		}
 	});
 }
 
 const passwordPepper = "SeCretPeppa4MySal+";
 function addUser(postData, callback) {
-	let sqlInsertSalt = "INSERT INTO web_user (first_name, last_name, email, password_salt) VALUES(: first_name, : last_name, : email, sha2(UUID(), 512)); ";
+	let sqlInsertSalt = "INSERT INTO web_user (first_name, last_name, email, password_salt)VALUES (:first_name, :last_name, :email, sha2(UUID(),512));";
 	let params = {
 		first_name: postData.first_name,
 		last_name: postData.last_name,
@@ -27,15 +27,14 @@ function addUser(postData, callback) {
 		if (err) {
 			console.log(err);
 			callback(err, null);
-		}
-		else {
+		} else {
 			let insertedID = results.insertId;
-			let updatePasswordHash = "UPDATE web_user SET password_hash = sha2(concat(: password,: pepper, password_salt), 512) WHERE web_user_id = : userId; "
+			let updatePasswordHash = "UPDATE web_user SET password_hash = sha2(concat(:password,:pepper,password_salt),512) WHERE web_user_id = :userId;";
 			let params2 = {
 				password: postData.password,
 				pepper: passwordPepper,
 				userId: insertedID
-			}
+			};
 			console.log(updatePasswordHash);
 			database.query(updatePasswordHash, params2, (err, results, fields) => {
 				if (err) {
@@ -48,7 +47,27 @@ function addUser(postData, callback) {
 				}
 			});
 		}
+	}
+	);
+}
+
+function deleteUser(webUserId, callback) {
+	let sqlDeleteUser = "DELETE FROM web_user WHERE web_user_id = :userID";
+	let params = { userID: webUserId };
+	console.log(sqlDeleteUser);
+	database.query(sqlDeleteUser, params, (err, results, fields) => {
+		if (err) {
+			callback(err, null);
+		}
+		else {
+			console.log(results);
+			callback(null, results);
+		}
 	});
 }
 
-module.exports = {getAllUsers, addUser}
+module.exports = {
+	getAllUsers,
+	addUser,
+	deleteUser
+}
